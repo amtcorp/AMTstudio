664e0b7003bf1c17a229f77262ca067d2a4e1ea8
amtcorp <141008517+amtcorp@users.noreply.github.com>
2023-11-22 13:26:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-    // Fonction pour détecter si l'utilisateur utilise un appareil mobile
-    function detectMobileDevice() {
-      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    }
-
-    // Redirection en fonction de la détection d'appareil mobile
-    window.onload = function() {
-      if (detectMobileDevice()) {
-        window.location.href = './mobile/index-mobile.html';
-      } else {
-      }
-    };
-
-
+   
 document.getElementById('startbutton').addEventListener('click', function() {
       const sectionPosition = document.getElementById('section1').offsetTop;
       window.scrollTo({
